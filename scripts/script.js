cab9ffab8e21e2a6f39bd64aef254ccc80278bbf
Sylvain Laporte <laporte.sylvain@outlook.com>
2019-08-12 18:54:05 -0400

    Add submenus

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,46 @@
+// author: Sylvain Laporte
+// program: menu.js
+// date: 2019-08-08
+// object: Implements menus for desktop and mobile.
+// reference: https://cssanimation.rocks/scroll-animations/
+
+
+// Condition for displaying desktop/mobile menu
+let mediaQuery = "(max-width: 989px)";
+
+
+/* ==================================================
+    Functions for toggling sub menus
+================================================== */ 
+
+/**
+ * Handler for toggling sub menus.
+ * @function toggleSubMenu
+ */
+function toggleSubMenu() {
+    this.nextElementSibling.classList.toggle("active");
+};
+
+/**
+ * Toggle sub menus.
+ * @function expandSubMenu
+ * @param {bool} action - Trigger adding and removing event listeners.
+ */
+function expandSubMenu(action) {
+    let elementsWithSubMenu = document.getElementsByClassName("with-sub-menu");
+
+    if (action) {
+        for (var i = 0; i < elementsWithSubMenu.length; i++) {
+            elementsWithSubMenu[i].addEventListener("click", toggleSubMenu, false);
+        };
+    } else {
+        // SOLVED listener not removing. Because new var toggleSubMenu when double calling expandSubMenu.
+        for (var j = 0; j < elementsWithSubMenu.length; j++) {
+            elementsWithSubMenu[j].removeEventListener("click", toggleSubMenu, false);
+        };
+    };
+};
+
 /* ==============================================
     Collapsible navigation menu for mobile
 ============================================== */
@@ -7,48 +50,14 @@
  * @function toggleNavMenu
  */
 function toggleNavMenu() {
-    var navButton = document.getElementById("nav-btn");
-    var navMenu = document.getElementsByClassName("nav-menu");
-
-    /* ==================================================
-        Functions for toggling sub menus
-    ================================================== */ 
-
-    /**
-     * Handler for toggling sub menus.
-     * @function toggleSubMenu
-     */
-    var toggleSubMenu = function() {
-        this.nextElementSibling.classList.toggle("active");
-    };
-
-    /**
-     * Toggle sub menus.
-     * @function expandSubMenu
-     * @param {bool} action - Trigger adding and removing event listeners.
-     */
-    var expandSubMenu = function(action) {
-        var elementsWithSubMenu = document.getElementsByClassName("with-sub-menu");
-    
-        if (action) {
-            for (var i = 0; i < elementsWithSubMenu.length; i++) {
-                elementsWithSubMenu[i].addEventListener("click", toggleSubMenu, false);
-            };
-        } else {
-            // SOLVED listener not removing. Because new var toggleSubMenu when double calling expandSubMenu.
-            for (var j = 0; j < elementsWithSubMenu.length; j++) {
-                elementsWithSubMenu[j].removeEventListener("click", toggleSubMenu, false);
-            };
-        };
-    };
-
-    // ==================================================
+    let navButton = document.getElementById("nav-btn");
+    let navMenu = document.getElementsByClassName("nav-menu");
 
     /**
      * Handler for opening main menu.
      * @function openMenu
      */
-    var openMenu = function() {
+    const openMenu = function() {
         navMenu[0].classList.add("active");
         event.stopPropagation();
         document.addEventListener("click", closeMenu, false);
@@ -60,7 +69,7 @@ function toggleNavMenu() {
      * Handler for closing main menu.
      * @function closeMenuà
      */
-    var closeMenu = function() {
+    const closeMenu = function() {
         if (event.target.closest(".nav-menu")) {
             return;
         } else {
@@ -74,4 +83,36 @@ function toggleNavMenu() {
     navButton.addEventListener("click", openMenu, false);
 };
 
-toggleNavMenu();
+
+/* ==============================================
+    Handling media queries
+============================================== */
+
+// Create a MediaQueryList object with a width condition.
+let mql = window.matchMedia(mediaQuery);
+
+/**
+ * Handler for the media change.
+ * @function handleMediaChange
+ * @param (object) - A MediaQueryList object.
+ */
+var handleMediaChange = function(mediaQueryList) {
+    // console.log(mql);
+    if (mediaQueryList.matches) {
+        toggleNavMenu();
+        // console.log("Matches");
+    } else {
+        expandSubMenu(true);
+        // console.log("Doesn't match");
+    }
+}
+
+// Run a first handleMediaChange on load.
+handleMediaChange(mql);
+
+// Add an event listener to the mql object for subsequent changes.
+mql.addListener(handleMediaChange);
+
+
+
+
